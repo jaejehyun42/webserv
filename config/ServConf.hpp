ceda3564ce1b8a2c8d81836093553cce71accc9c
OortC <jaebin.lee00@gmail.com>
2025-01-03 19:44:13 +0900

    feat: Write parse function for config file configuration file 파싱을 위한 함수 작성 완료 및 정상 작동 확인
    동작 방식:
    Block과 Key/Value 두 가지 경우로 구분하여 파일 내용을 확인
    (1) Block
    	어떤 블럭인지 확인한 후, 허용되는 블럭이면 하위 함수로 들어가서 파싱
    	허용되지 않은 블럭이면 에러 처리
    (2) Key/Value
    	맨 끝이 ;으로 끝나는지 확인, 아니면 에러 처리
    	키값이 허용되는 config 디렉티브 일 경우, 맴버 변수에 넣어줌
    	허용되지 않는 디렉티브이면 에러 처리

--- config/ServConf.hpp
+++ config/ServConf.hpp
@@ -4,33 +4,47 @@
 #include <map>
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <exception>
 
-typedef struct location
-{
-	bool autoindex;
-	std::string index;
-	std::string cgiPass;
-} location;
+#define GET 0
+#define POST 1
+#define DELETE 2
 
-typedef struct server
+typedef struct s_location
+{
+	bool method[3]; // 인덱스 순서는 GET, POST, DELETE 순서
+	bool autoindex;
+	std::string cgiPass;
+
+	std::vector<std::string> index; // index가 여러개 들어가는 경우 생각해야됨
+} t_location;
+
+typedef struct s_serv
 {
 	int port;
 	int maxSize;
-	std::string name;
 	std::string root;
+	std::string name;
 
-	std::map<std::string, std::string> error;
-	std::map<std::string, struct location> path;
-} server;
+	std::map<long, std::string> error;
+	std::map<std::string, struct s_location> path;
+} t_serv;
 
 class ServConf
 {
 private:
-	int aliveTime;
+	long aliveTime;
 	std::map<std::string, std::string> mime;
-	std::vector<struct server> serv;
+	std::vector<struct s_serv> serv;
+
+	void includeFile(const std::string& fileName);
+
+	void parseMime(std::ifstream& file);
+	void parseHTTP(std::ifstream& file, bool inc);
+	struct s_serv parseServ(std::ifstream& file);
+	struct s_location parsePath(std::ifstream& file, const std::string& path);
 public:
 	ServConf();
 	ServConf(const ServConf& ref);
@@ -38,16 +52,8 @@ public:
 	~ServConf();
 
 	void parse(const std::string& fileName);
-	void setConf();
 
-	int getAliveTime() const;
-	const std::string& getContentType(const std::string& ext) const;
-
-	int getServSize() const;
-	int getServPort(int idx) const;
-
-	const std::string& getErrorPage(int status) const;
-	const std::string& getPath(const std::string& path);
+	void print() const; // 제대로 넣어졌는지 체크 용도
 };
 
 #endif
