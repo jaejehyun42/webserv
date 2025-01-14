12cdb067c7b6931d10e2e67318d65dd8a82b792f
Jeong-su Shin <jeshin@c5r8s6.42seoul.kr>
2025-01-14 15:12:02 +0900

    chore: add content type and length in error page

--- response/ErrorResponse.cpp
+++ response/ErrorResponse.cpp
@@ -8,28 +8,45 @@ ErrorResponse::ErrorResponse(){}
 ErrorResponse::~ErrorResponse(){}
 
 void    ErrorResponse::setMessage(const std::unordered_map<int, std::string>& data){
-    _message = "HTTP/1.1 " + data.at(__statusCode) + " " + data.at(__reasonPhrase) + "\r\n\r\n";
 
+    _message = "HTTP/1.1 " + data.at(__statusCode) + " " + data.at(__reasonPhrase) + "\r\n";
+    _message += "Content-Type: " + data.at(__contentType) + "\r\n";
     if (data.at(__path).empty()){
-        _message += "<!DOCTYPE html>\n"
+        std::string tmpMessage;
+        std::ostringstream oss;
+
+        tmpMessage += "<!DOCTYPE html>\n"
                 "<head>\n"
-                    "<title>" + data.at(__statusCode) + " " + data.at(__reasonPhrase) +"</title>\n"
+                    "   <title>" + data.at(__statusCode) + " " + data.at(__reasonPhrase) +"</title>\n"
                 "</head>\n"
                 "<body>\n"
-                    "<h1>404</h1>\n"
-                    "<p>" + data.at(__statusCode) + data.at(__reasonPhrase) + "</p>\n"
+                    "   <h1>404</h1>\n"
+                    "   <p>" + data.at(__statusCode) + data.at(__reasonPhrase) + "</p>\n"
                 "</body>\n"
                 "</html>";
+        
+        oss<<tmpMessage.size();
+        _message += "Content-Length: " + oss.str() + "\r\n";
+        _message += tmpMessage;
     }else{
         std::ifstream ifs(data.at(__path), std::ios::binary);
-        if (!ifs)
-            std::cerr<<"Error: error page\n";
+        std::ostringstream oss;
+        if (!ifs){
+            std::cerr<<"Error: error page\n"; //여기서 에러나면? 
+        }
+
         ifs.seekg(0, std::ios::end);
         std::streampos size = ifs.tellg();
         ifs.seekg(0, std::ios::beg);
+
+        oss<<size;
+        _message += "Content-Length: " + oss.str() + "\r\n";
+
         std::string buf(size, '\0');
-        ifs.read(&buf[0], size);
+        ifs.read(&buf[0], size); //에러나면? 
+
         _message += buf;
+
         ifs.close();
     }
 }
