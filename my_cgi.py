ca50dd36bc5fc598f6f8feaf105190b9a63d4f39
Seung Hyun Han <136709860+tmdgus1dnl@users.noreply.github.com>
2025-01-16 12:28:01 +0900

    fix: change "content_length" var type to int

--- my_cgi.py
+++ my_cgi.py
@@ -16,20 +16,22 @@ def chkError(e):
         return "500 Internal Server Error"
 
 # 환경 변수에서 각종 정보 읽기
-#path_info = os.environ.get("PATH_INFO", "")
-#query_string = os.environ.get("QUERY_STRING", "")
-#request_method = os.environ.get("REQUEST_METHOD", "")
-#content_type   = os.environ.get("CONTENT_TYPE", "")
-#content_length = os.environ.get("CONTENT_LENGTH", 0)
-#root_dir = os.environ.get("DOCUMENT_ROOT", "")
+path_info = os.environ.get("PATH_INFO", "")
+query_string = os.environ.get("QUERY_STRING", "")
+request_method = os.environ.get("REQUESTED_METHOD", "")
+content_type   = os.environ.get("CONTENT_TYPE", "")
+content_length = os.environ.get("CONTENT_LENGTH", 0)
+root_dir = os.environ.get("DOCUMENT_ROOT", "")
 
-path_info = "/now_files"
+#path_info = "/test.txt"
 #query_string = "name=John&&age=30&hobby=reading&hobby=traveling"
-query_string = ""
-request_method = "DELETE"
-content_length = 0
-content_type = "text/plain"
-root_dir = "/Users/seunghan/42_projects/webserv/working/CGI"
+#query_string = ""
+#request_method = "GET"
+#content_length = 0
+#content_type = "text/plain"
+#root_dir = "/Users/seunghan/42_projects/webserv/debug/d1/response/cgifiles"
+
+content_length = int(content_length)
 
 # HTML 제목
 title = ""
